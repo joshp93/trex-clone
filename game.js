a38fe62b2b1b6590393c4ce5a76c152959b5f40e
Joshua Pearson <joshua.pearson@nationalexpress.com>
2024-04-17 15:50:04 +0100

    Dino working and clouds working

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-import { background, dino, setCanvasSizes, DINO_STANCES } from "./modules/canvas.js";
+import { background, dino, setCanvasSizes, DINO_STANCES, clouds, cloudObject } from "./modules/canvas.js";
 
 let keyState = {};
 
@@ -6,11 +6,11 @@ addEventListener("resize", () => {
     setCanvasSizes();
 });
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
     keyState[event.code] = true;
 });
 
-document.addEventListener('keyup', function(event) {
+document.addEventListener('keyup', function (event) {
     keyState[event.code] = false;
 });
 
@@ -21,53 +21,109 @@ setCanvasSizes();
 background.draw();
 
 function game() {
-    if (keyState['Space'] === true && dino.maxHeightReached === false) {
-        increaseJumpHeight();
+    if (keyState['Space'] === true && dino.jumpCancelled === false) {
+        jump();
     } else {
         stopJumpingAndReturnToGround();
     }
-
+    updateClouds();
 
     dino.draw();
+    clouds.draw();
     raf = window.requestAnimationFrame(game);
 }
 
-function increaseJumpHeight() {
+function jump() {
     dino.stance = DINO_STANCES.JUMP;
-    if (dino.verticalPosition === 0) {
-        dino.maxHeightReached = true;
+    dino.jumpStarted = true;
+    if (dino.maxHeightReached === true) {
+        stopJumpingAndReturnToGround();
         return;
     }
+    if (dino.verticalPosition <= dino.MAX_HEIGHT) {
+        dino.maxHeightReached = true;
+        stopJumpingAndReturnToGround();
+        return;
+    }
+    increaseHeight();
+}
+
+function increaseHeight() {
     dino.verticalPosition -= dino.jumpSpeed;
     if (dino.verticalPosition < 0) {
         dino.verticalPosition = 0;
     }
+}
+
+function slowDownJump() {
     if (dino.jumpSpeed > 1) {
         dino.jumpSpeed -= (dino.jumpSpeed * dino.ACCELARTION_FACTOR);
+        increaseHeight();
+    } else {
+        dino.drifting = false;
     }
 }
 
 function stopJumpingAndReturnToGround() {
-    if (dino.maxHeightReached === false && dino.verticalPosition !== dino.INITIAL_POSITION) {
-        increaseJumpHeight();
+    if ((keyState['Space'] === true && dino.maxHeightReached === true && dino.jumpCancelled === false) || (dino.drifting === true && dino.jumpCancelled === true)) {
+        slowDownJump();
+        if (dino.verticalPosition === 0) {
+            dino.jumpCancelled = true;
+        }
+        return;
+    }
+
+    if (keyState['Space'] === false && dino.maxHeightReached === false && dino.jumpCancelled === false && dino.jumpStarted === true) {
+        dino.drifting = true;
+        dino.jumpCancelled = true;
         return;
     }
 
     if (dino.verticalPosition === dino.INITIAL_POSITION) {
         if (dino.stance === DINO_STANCES.JUMP) {
             dino.stance = DINO_STANCES.STAND_BOTH;
+            dino.jumpSpeed = dino.INITIAL_JUMP_SPEED;
+            dino.jumpCancelled = false;
+            dino.jumpStarted = false;
         }
-        dino.maxHeightReached = false;
         return;
     }
+    decreaseHeight();
+    speedUpFall();
+}
+
+function decreaseHeight() {
     dino.verticalPosition += dino.jumpSpeed;
     if (dino.verticalPosition > dino.INITIAL_POSITION) {
         dino.verticalPosition = dino.INITIAL_POSITION;
     }
+    dino.maxHeightReached = false;
+}
+
+function speedUpFall() {
     if (dino.jumpSpeed < dino.INITIAL_JUMP_SPEED) {
         dino.jumpSpeed += (dino.jumpSpeed * dino.ACCELARTION_FACTOR);
-        if (dino.jumpSpeed > dino.INITIAL_JUMP_SPEED) {
-            dino.jumpSpeed = dino.INITIAL_JUMP_SPEED;
+    }
+}
+
+function buildCloud() {
+    const cloud = JSON.parse(JSON.stringify(cloudObject));
+    cloud.x = window.innerWidth;
+    cloud.y = Math.floor(Math.random() * ((window.innerHeight / 3) - 50)) + 20;
+    cloud.speedMultiplier = Math.floor(Math.random() * 3) + 1;
+    return cloud;
+}
+
+function updateClouds() {
+    if (clouds.array.length === 0) {
+        clouds.array.push(buildCloud());
+    }
+
+    for (let i = 0; i < clouds.array.length; i++) {
+        clouds.array[i].x -= 1 * clouds.array[i].speedMultiplier;
+
+        if (clouds.array[i].x <= 0 - 100) {
+            clouds.array.splice(i, 1);
         }
     }
 }
@@ -83,4 +139,12 @@ setInterval(() => {
     } else {
         dino.stance = DINO_STANCES.STAND_LEFT;
     }
+
+    clouds.array.forEach(cloud => cloud.x++);
 }, 150);
+
+setInterval(() => {
+    if (clouds.array.length < 4 && (Math.floor(Math.random() * 10) + 1) > 6) {
+        clouds.array.push(buildCloud());
+    }
+}, 1000);
--- modules/canvas.js
+++ modules/canvas.js
@@ -1,5 +1,68 @@
 const backgroundCanvas = document.getElementById("background");
 const dinoCanvas = document.getElementById("dino");
+const cloudsCanvas = document.getElementById("clouds");
+
+const PRIMARY_COLOR = "rgb(84, 84, 84)";
+
+const drawRect = (ctx, x, y, w, h, verticalDrawDistanceIncrease, addVerticalFiller = false) => {
+    ctx.fillRect(x, y, w, addVerticalFiller ? h + 1 : h);
+    if (verticalDrawDistanceIncrease) {
+        return verticalDrawDistanceIncrease;
+    } else {
+        return 0;
+    }
+};
+
+const clouds = {
+    color: "grey",
+    array: [],
+    drawCloud(cloud) {
+        let verticalDrawDistance = cloud.y;
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 25, verticalDrawDistance, 4, 1, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 20, verticalDrawDistance, 6, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 19, verticalDrawDistance, 2, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 30, verticalDrawDistance, 2, 1, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 17, verticalDrawDistance, 3, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 31, verticalDrawDistance, 1, 1, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 17, verticalDrawDistance, 1, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 31, verticalDrawDistance, 4, 1, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 17, verticalDrawDistance, 1, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 31, verticalDrawDistance, 1, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 34, verticalDrawDistance, 6, 1, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 14, verticalDrawDistance, 4, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 30, verticalDrawDistance, 1, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 39, verticalDrawDistance, 1, 1, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 13, verticalDrawDistance, 2, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 6, verticalDrawDistance, 8, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 42, verticalDrawDistance, 1, 1, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 5, verticalDrawDistance, 2, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 42, verticalDrawDistance, 3, 1, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 5, verticalDrawDistance, 1, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 44, verticalDrawDistance, 1, 1, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 1, verticalDrawDistance, 2, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 4, verticalDrawDistance, 2, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 9, verticalDrawDistance, 1, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 45, verticalDrawDistance, 1, 1, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x, verticalDrawDistance, 2, 1);
+        verticalDrawDistance += drawRect(cloudsCtx, cloud.x + 10, verticalDrawDistance, 36, 1);
+    },
+
+    draw() {
+        cloudsCtx.clearRect(0, 0, cloudsCanvas.width, cloudsCanvas.height);
+        cloudsCtx.fillStyle = this.color;
+
+        this.array.forEach(cloud => this.drawCloud(cloud));
+    }
+}
+
+const cloudObject = {
+    x: 0,
+    y: 9,
+    w: 16,
+    h: 13,
+    speedMultiplier: 1,
+}
+
 const background = {
     x: 0,
     y: 0,
@@ -21,7 +84,6 @@ const DINO_STANCES = {
     DUCK: 5
 };
 
-const PRIMARY_COLOR = "rgb(84, 84, 84)";
 
 const dino = {
     x: 0,
@@ -35,87 +97,82 @@ const dino = {
     verticalPosition: 140,
     INITIAL_POSITION: 140,
     INITIAL_JUMP_SPEED: 15,
-    jumpSpeed: 15,
-    ACCELARTION_FACTOR: 0.05,
-    maxHeightReached: false, 
+    jumpSpeed: 20,
+    ACCELARTION_FACTOR: 0.2,
+    MAX_HEIGHT: 60,
+    maxHeightReached: false,
+    drifting: false,
+    jumpCancelled: false,
+    jumpStarted: false,
     draw() {
-        const drawRect = (x, y, w, h, verticalDrawDistanceIncrease) => {
-            dinoCtx.fillRect(this.dinoPadding + x, y, w, h);
-            if (verticalDrawDistanceIncrease) {
-                return verticalDrawDistanceIncrease;
-            } else {
-                return 0;
-            }
-        };
-
         const drawHead = (verticalDrawDistance) => {
             const drawEye = (verticalDrawDistance) => {
                 dinoCtx.fillStyle = "red";
-                drawRect(37, verticalDrawDistance, 3, 3);
+                drawRect(dinoCtx, this.dinoPadding + 37, verticalDrawDistance, 3, 3);
                 dinoCtx.fillStyle = this.color;
                 return 0;
             }
 
             dinoCtx.fillStyle = this.color;
-            verticalDrawDistance += drawRect(34, verticalDrawDistance, 25, 3, 3);
-            verticalDrawDistance += drawRect(31, verticalDrawDistance, 6, 20);
+            verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 34, verticalDrawDistance, 25, 3, 3, true);
+            verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 31, verticalDrawDistance, 6, 20, true);
             verticalDrawDistance += drawEye(verticalDrawDistance);
-            verticalDrawDistance += drawRect(40, verticalDrawDistance, 22, 3, 3);
-            verticalDrawDistance += drawRect(37, verticalDrawDistance, 26, 10, 10);
-            verticalDrawDistance += drawRect(37, verticalDrawDistance, 10, 6, 3);
-            verticalDrawDistance += drawRect(47, verticalDrawDistance, 9, 3);
+            verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 40, verticalDrawDistance - 1, 22, 4, 3, true);
+            verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 37, verticalDrawDistance, 26, 10, 10, true);
+            verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 37, verticalDrawDistance, 10, 6, 3, true);
+            verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 47, verticalDrawDistance, 9, 4);
             return verticalDrawDistance;
         };
 
         const drawBody = (verticalDrawDistance) => {
-            verticalDrawDistance += drawRect(28, verticalDrawDistance, 16, 3, 3);
-            verticalDrawDistance += drawRect(24, verticalDrawDistance, 20, 3, 3);
-            verticalDrawDistance += drawRect(19, verticalDrawDistance, 25, 3, 3);
-            verticalDrawDistance += drawRect(15, verticalDrawDistance, 29, 3, 3);
-            verticalDrawDistance += drawRect(12, verticalDrawDistance, 32, 7, 7);
-            verticalDrawDistance += drawRect(12, verticalDrawDistance, 27, 3, 3);
-            verticalDrawDistance += drawRect(12, verticalDrawDistance, 24, 3, 3);
-            verticalDrawDistance += drawRect(12, verticalDrawDistance, 21, 3, 3);
+            verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 28, verticalDrawDistance, 16, 3, 3, true);
+            verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 24, verticalDrawDistance, 20, 3, 3, true);
+            verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 19, verticalDrawDistance, 25, 3, 3, true);
+            verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 15, verticalDrawDistance, 29, 3, 3, true);
+            verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 12, verticalDrawDistance, 32, 7, 7, true);
+            verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 12, verticalDrawDistance, 27, 3, 3, true);
+            verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 12, verticalDrawDistance, 24, 3, 3, true);
+            verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 12, verticalDrawDistance, 21, 3, 3);
             return verticalDrawDistance;
         };
 
         const drawTail = (verticalDrawDistance) => {
-            verticalDrawDistance += drawRect(0, verticalDrawDistance, 3, 6, 6);
-            verticalDrawDistance += drawRect(0, verticalDrawDistance, 6, 3, 3);
-            verticalDrawDistance += drawRect(0, verticalDrawDistance, 9, 3, 3);
-            verticalDrawDistance += drawRect(0, verticalDrawDistance, 13, 5, 5);
-            verticalDrawDistance += drawRect(3, verticalDrawDistance, 10, 3, 3);
-            verticalDrawDistance += drawRect(6, verticalDrawDistance, 7, 4, 4);
+            verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 0, verticalDrawDistance, 3, 6, 6, true);
+            verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 0, verticalDrawDistance, 6, 3, 3, true);
+            verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 0, verticalDrawDistance, 9, 3, 3, true);
+            verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 0, verticalDrawDistance, 13, 5, 5, true);
+            verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 3, verticalDrawDistance, 10, 3, 3, true);
+            verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 6, verticalDrawDistance, 7, 4, 4);
             return verticalDrawDistance;
         };
 
         const drawArms = (verticalDrawDistance) => {
-            verticalDrawDistance += drawRect(44, verticalDrawDistance, 6, 3, 3);
-            verticalDrawDistance += drawRect(47, verticalDrawDistance, 3, 3, 3);
+            verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 44, verticalDrawDistance, 6, 3, 3);
+            verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 47, verticalDrawDistance, 3, 3, 3);
             return verticalDrawDistance;
         };
 
         const drawLegs = (verticalDrawDistance, stance) => {
             const drawLeftLeg = (verticalDrawDistance, stance) => {
                 if (stance === DINO_STANCES.STAND_BOTH || stance === DINO_STANCES.STAND_RIGHT || stance === DINO_STANCES.JUMP) {
-                    verticalDrawDistance += drawRect(15, verticalDrawDistance, 9, 3, 3);
-                    verticalDrawDistance += drawRect(15, verticalDrawDistance, 6, 3, 3);
-                    verticalDrawDistance += drawRect(15, verticalDrawDistance, 3, 3, 3);
-                    verticalDrawDistance += drawRect(15, verticalDrawDistance, 7, 3, 3);
+                    verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 15, verticalDrawDistance, 9, 3, 3);
+                    verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 15, verticalDrawDistance, 6, 3, 3);
+                    verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 15, verticalDrawDistance, 3, 3, 3);
+                    verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 15, verticalDrawDistance, 7, 3, 3);
                 } else {
-                    verticalDrawDistance += drawRect(15, verticalDrawDistance, 3, 3, 3);
-                    verticalDrawDistance += drawRect(15, verticalDrawDistance, 7, 3, 3);
+                    verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 15, verticalDrawDistance, 3, 3, 3);
+                    verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 15, verticalDrawDistance, 7, 3, 3);
                 }
             };
 
             const drawRightLeg = (verticalDrawDistance, stance) => {
                 if (stance === DINO_STANCES.STAND_BOTH || stance === DINO_STANCES.STAND_LEFT || stance === DINO_STANCES.JUMP) {
-                    verticalDrawDistance += drawRect(28, verticalDrawDistance, 5, 3, 3);
-                    verticalDrawDistance += drawRect(30, verticalDrawDistance, 3, 6, 6);
-                    verticalDrawDistance += drawRect(30, verticalDrawDistance, 7, 3, 3);
+                    verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 28, verticalDrawDistance, 5, 3, 3);
+                    verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 30, verticalDrawDistance, 3, 6, 6);
+                    verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 30, verticalDrawDistance, 7, 3, 3);
                 } else {
-                    verticalDrawDistance += drawRect(30, verticalDrawDistance, 3, 3, 3);
-                    verticalDrawDistance += drawRect(30, verticalDrawDistance, 7, 3, 3);
+                    verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 30, verticalDrawDistance, 3, 3, 3);
+                    verticalDrawDistance += drawRect(dinoCtx, this.dinoPadding + 30, verticalDrawDistance, 7, 3, 3);
                 }
             };
 
@@ -142,13 +199,17 @@ const dino = {
 
 const backgroundCtx = backgroundCanvas.getContext("2d");
 const dinoCtx = dinoCanvas.getContext("2d");
+const cloudsCtx = cloudsCanvas.getContext("2d");
 
 function setCanvasSizes() {
     setCanvasSize(backgroundCanvas, window.innerWidth, window.innerHeight, 0, 0);
+
     const dinoHalfHeight = 200 / 2;
     const dinoTop = Math.floor((window.innerHeight / 2) - (dinoHalfHeight));
     dino.canvasWidth = window.innerWidth;
     setCanvasSize(dinoCanvas, dino.canvasWidth, 200, 0, dinoTop);
+
+    setCanvasSize(cloudsCanvas, window.innerWidth, window.innerHeight / 3, 0, 0)
 }
 
 function setCanvasSize(canvas, width, height, left, top) {
@@ -158,4 +219,4 @@ function setCanvasSize(canvas, width, height, left, top) {
     canvas.style.left = `${left}px`;
 }
 
-export { background, dino, setCanvasSizes, DINO_STANCES };
+export { background, dino, setCanvasSizes, DINO_STANCES, clouds, cloudObject };
